bdf356aa9fc38a7b30ac8f586e05b5824229f511
Leonard Colin <leooo.colin@gmail.com>
2020-12-08 17:45:33 +0100

    Create class Parser with process method to parse user's question

--- gpapp/utils/parser.py
+++ gpapp/utils/parser.py
@@ -1,35 +1,29 @@
 from typing import List
 
-from constants import STOP_WORDS, ADDITIONAL_STOP_WORDS
+from constants import STOP_WORDS, ADDITIONAL_STOP_WORDS, PUNCTUATION
 
 
 class Parser:
     """ Class that ..."""
 
-    def __init__(self, message: str):
+    def __init__(self):
         """ Class constructor """
+        pass
 
-        self.message = message
-        # self.process()
-
-    def process(self) -> str:
+    def process(self, message) -> str:
         """ Method that returns only meaningful words from the user's sentence """
-
         keywords = []
-        message = self.message.lower()
-        message = message.replace("'", " ").replace("é", "e").replace("ê", "e").replace("è", "e")
-        message = message.split(" ")
-        for word in message:
-            if word not in STOP_WORDS and word not in ADDITIONAL_STOP_WORDS:
-                keywords.append(word)
-        keywords = " ".join(keywords)
-
-        if keywords == " " or keywords == "":
-            raise ValueError("Please reformulate your question")
-
-        print(keywords)
-        return keywords
-
-
-# if __name__ == '__main__':
-#     Parser(message="Ok mais quoi alors ? ? !")
+        try:
+            message = message.lower()
+            for chars in PUNCTUATION:
+                if chars in message:
+                    message = message.replace(chars, " ")
+            message = message.split(" ")
+            for word in message:
+                if word not in STOP_WORDS and word not in ADDITIONAL_STOP_WORDS:
+                    keywords.append(word)
+            keywords = " ".join(keywords)
+            print(keywords)
+            return keywords
+        except ValueError:
+            return keywords
