cfd4d222c4de1a7ae9a73f89e60ae3eec1ae3439
Leonard Colin <leooo.colin@gmail.com>
2020-12-09 16:26:15 +0100

    Refactor views.py

--- gpapp/views.py
+++ gpapp/views.py
@@ -1,16 +1,17 @@
 from flask import render_template, jsonify, request
 
 from . import app
-from .utils.parser import Parser as par
+from gpapp.grandpy import GrandPy
 
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template("index.html")
 
 
-@app.route('/question', methods=["POST"])
-def question():
+@app.route("/grandpy", methods=["POST"])
+def place_info():
+    grandpy = GrandPy()
     user_text = request.form["userText"]
-    response = par(user_text).process()
+    response = grandpy.grandpy_answer(user_text)
     return jsonify(response)
