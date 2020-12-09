7d060ce0680089486e593798f0e4b93e5bf56ee5
Leonard Colin <leooo.colin@gmail.com>
2020-12-09 16:30:25 +0100

    Add Try Catch to googlemaps API

--- gpapp/utils/googlemaps_API.py
+++ gpapp/utils/googlemaps_API.py
@@ -1,31 +1,25 @@
 import requests
 
-from config import GOOGLE_API_KEY
-from gpapp.utils.parser import Parser
+from gpapp.utils.config import GOOGLE_API_KEY
 
 
 class GoogleMapsApi:
 
     def __init__(self):
-        self.get_place_location(keywords="tour eiffel")
         pass
 
     def get_place_location(self, keywords):
-        # try:
-        url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json"
-        params = {
-            "inputtype": "textquery",
-            "input": keywords,
-            "fields": "formatted_address,geometry/location",
-            "key": GOOGLE_API_KEY
-        }
-        data = requests.get(url, params)
-        response = data.json()['candidates'][0]
-        print(response)
-        return response
-    # except IndexError:
-    #     return None
-
-
-if __name__ == '__main__':
-    GoogleMapsApi()
+        try:
+            url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json"
+            params = {
+                "inputtype": "textquery",
+                "input": keywords,
+                "fields": "formatted_address,geometry/location",
+                "key": GOOGLE_API_KEY
+            }
+            data = requests.get(url, params)
+            response = data.json()['candidates'][0]
+            print(response)
+            return response
+        except IndexError:
+            return None
