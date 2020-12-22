59478fa77602e9f288dd91e3119930bc64b1b052
Leonard Colin <leooo.colin@gmail.com>
2020-12-22 15:58:43 +0100

    Make it better
    Adding language parameter for Gmaps API

--- gpapp/utils/googlemaps_API.py
+++ gpapp/utils/googlemaps_API.py
@@ -11,6 +11,7 @@ class GoogleMapsApi:
 
     def __init__(self):
         """Class constructor"""
+        # self.get_place_location(keywords="mlkzeadaze:;d,eza:;d,aze:;d,")
         pass
 
     def get_place_location(self, keywords: str):
@@ -19,17 +20,22 @@ class GoogleMapsApi:
         its address, latitude and longitude
         """
 
-        try:
-            url = "https://maps.googleapis.com/maps/api/" \
-                  "place/findplacefromtext/json"
-            params = {
-                "inputtype": "textquery",
-                "input": keywords,
-                "fields": "formatted_address,geometry/location",
-                "key": GOOGLE_API_KEY
-            }
-            data = requests.get(url, params)
-            response = data.json()['candidates'][0]
-            return response
-        except (IndexError, TypeError):
-            return None
+        # try:
+        url = "https://maps.googleapis.com/maps/api/" \
+              "place/findplacefromtext/json"
+        params = {
+            "inputtype": "textquery",
+            "input": keywords,
+            "fields": "formatted_address,geometry/location",
+            "key": GOOGLE_API_KEY,
+            "language": "fr"
+        }
+        data = requests.get(url, params)
+        response = data.json()['candidates'][0]
+        return response
+    # except (IndexError, TypeError):
+        #     return None
+
+
+# if __name__ == '__main__':
+#     GoogleMapsApi()
